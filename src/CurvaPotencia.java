7cd4142b4bddfd34bd04f25c6dd3aefb39c7cfda
Pietro Zardini <pizardini@gmail.com>
2023-09-29 10:43:51 -0300

    mudança nos valores de potencia e correção de nomes

--- src/CurvaPotencia.java
+++ src/CurvaPotencia.java
@@ -1,16 +1,27 @@
-class CurvaDePotencia {
-    private double m;
-    private double n;
+class CurvaPotencia {
+    private double m1;
+    private double n1;
+    private int rpmPMax;
+    private double m2;
+    private double n2;
 
-    public CurvaDePotencia(double m, double n) {
-        this.m = m;
-        this.n = n;
+    public CurvaPotencia(double m1, double n1, int rpmPMax, double m2, double n2) {
+        this.m1 = m1;
+        this.n1 = n1;
+        this.rpmPMax = rpmPMax;
+        this.m2 = m2;
+        this.n2 = n2;
     }
 
-    public double calcularPotencia(int rotacao) {
-        if (rotacao < 0) {
+    public double calcularPotencia(int rpm) {
+        if (rpm < 0) {
             throw new IllegalArgumentException("Rotação não pode ser negativa.");
         }
-        return m * rotacao + n;
+
+        if (rpm <= rpmPMax) {
+            return m1 * rpm + n1; // Equação antes da potência máxima
+        } else {
+            return m2 * rpm + n2; // Equação após a potência máxima
+        }
     }
 }
--- src/CurvaTorque.java
+++ src/CurvaTorque.java
@@ -1,9 +1,9 @@
-class CurvaDeTorque {
+class CurvaTorque {
     private double a;
     private double b;
     private double c;
 
-    public CurvaDeTorque(double a, double b, double c) {
+    public CurvaTorque(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
