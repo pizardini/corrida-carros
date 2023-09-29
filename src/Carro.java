3ea0bcf627c4d5716a20539f903f18734e49a0e2
Pietro Zardini <pizardini@gmail.com>
2023-09-29 10:36:13 -0300

    criação de classes para curvas de torque e potência

--- src/Carro.java
+++ src/Carro.java
@@ -1,18 +1,20 @@
-import java.util.Objects;
-
 class Carro {
     private int numero;
     private String marca;
     private String modelo;
     private Categoria categoria;
     private int rpmPotenciaMaxima;
+    private CurvaTorque curvaTorque;
+    private CurvaPotencia curvaPotencia;
 
-    public Carro(int numero, String marca, String modelo, Categoria categoria, int rpmPotenciaMaxima) {
+    public Carro(int numero, String marca, String modelo, Categoria categoria, int rpmPotenciaMaxima, CurvaTorque curvaTorque, CurvaPotencia curvaPotencia) {
         this.numero = numero;
         this.marca = marca;
         this.modelo = modelo;
         this.categoria = categoria;
         this.rpmPotenciaMaxima = rpmPotenciaMaxima;
+        this.curvaTorque = curvaTorque;
+        this.curvaPotencia = curvaPotencia;
     }
 
 }
